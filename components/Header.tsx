85387c1f8f9dd366d0a038560774d57f104a1991
Abhiraj Thakur <abhirajthakur124@gmail.com>
2022-10-26 00:56:37 +0530

    add header component

---
+++ components/Header.tsx
@@ -0,0 +1,72 @@
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header
+      className="sticky top-0 p-5 flex items-start justify-between max-w-7xl z-50 
+    xl:items-center"
+    >
+      {/* Social Icons */}
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.3,
+        }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          url="https://twitter.com/AbhirajThakur21"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/abhiraj-thakur-1a53b9218/"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+      </motion.div>
+
+      {/* Contact Me */}
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.3,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor"
+      >
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get in Touch
+        </p>
+      </motion.div>
+    </header>
+  );
+};
+
+export default Header;
